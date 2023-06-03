bd5984e4e036fc0bfa75ca3c589a38168323a34b
robshahla <shahla.robert@gmail.com>
2023-06-03 17:51:00 +0300

    added pyshark usage

--- l4lb/loadBalancer.py
+++ l4lb/loadBalancer.py
@@ -1,26 +1,58 @@
 from scapy.all import *
 import socket
+import pyshark
+import pickle
+from pyshark.packet.common import Pickleable
+from pyshark.packet.packet import Packet
+from pyshark.packet.fields import LayerField, LayerFieldsContainer
 
 BACKEND_SERVER_IP = "172.18.2.2"
 BACKEND_SERVER_PORT = 6666
 
 counter = 0
+
+
 def handle_packet(packet):
-    #print("packet received")
+    print("packet received")
     global counter
+
+    # quic_packet = packet[UDP].payload
+    # quic_headers = quic_packet[Raw].load
+    quic_headers = packet
+    pickled_packet = pickle.loads(pickle.dumps(quic_headers, pickle.HIGHEST_PROTOCOL))
+    pyshark_packet = LayerFieldsContainer(pyshark.packet.packet.Packet(pickled_packet))
+    print(pyshark_packet)
+    # for layer in pyshark_packet:
+    #     print(layer.layer_name)
+    # print(LayerField(pyshark_packet).quic.frame)
+    # print("Packet #{}, {}".format(pickled_packet.highest_layer, pickled_packet._packet_string))
+
+    # parsed_packet = pyshark.packet.packet.from_string(str(pickled_packet))
+    # print(parsed_packet)
+
+    print(quic_headers)
+
+    # print(f'Index: {pyshark_packet.number}')
+    # print(f'Timestamp: {pyshark_packet.sniff_time}')
+    # print(f'Bytes: {pyshark_packet.length}')
+    # print(f'Layers: {pyshark_packet.layers}')
+
+    # for layer in packet:
+    #     if layer.layer_name == 'quic':
+
     if "IP" in packet:
         #print("updating IP dest address")
         #packet["IP"].dst = BACKEND_SERVER_IP
-        #print(packet["IP"].src)
-        #print(packet["IP"].dst)
+        print(packet["IP"].src)
+        print(packet["IP"].dst)
         ip = IP(src=packet[IP].src, dst=BACKEND_SERVER_IP)
 
 
     if "UDP" in packet:
         #print("updating UDP dest port")
         #packet["UDP"].dport = BACKEND_SERVER_PORT
-        #print(packet["UDP"].sport)
-        #print(packet["UDP"].dport)
+        print(packet["UDP"].sport)
+        print(packet["UDP"].dport)
         udp = UDP(sport=packet[UDP].sport, dport=BACKEND_SERVER_PORT)
 
     data = packet[Raw].load
@@ -29,8 +61,8 @@ def handle_packet(packet):
     sendp(newp, iface="eth0", verbose=0)
 
     counter += 1
-    #print("counter value: ", counter)
-    #print("------------------------")
+    print("counter value: ", counter)
+    print("------------------------")
     #packet["Ether"].dst = "02:42:ac:12:00:03"
     #packet["Ether"].src = "02:42:ac:12:01:02"
     # eth1 is the interface that is connected to the backend server
@@ -54,10 +86,11 @@ def main():
     #p = IP(src="172.19.0.2", dst="172.18.0.3")/ICMP()
     #send(p)
 
-    sniff(filter='port 8888', prn=handle_packet, timeout=60)
+    sniff(filter='port 8888', prn=handle_packet)
     global counter
     print("counter value: ", counter)
     print("finished sniffing")
+
 
 if __name__ == '__main__':
     main()
